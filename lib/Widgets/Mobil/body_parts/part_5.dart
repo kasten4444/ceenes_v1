098ce94d9cc3134fe1f0ce204b51da7ef7246beb
Lorenz <enzolop6@gmail.com>
2020-11-29 20:18:04 +0100

    mobile fixed

---
+++ lib/Widgets/Mobil/body_parts/part_5.dart
@@ -0,0 +1,197 @@
+import 'package:ceenes/assets/Styles/Colors/colors.dart';
+import 'package:ceenes/assets/Styles/Icons/my_icons.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
+
+
+class Part5 extends StatefulWidget {
+  @override
+  _Part5State createState() => _Part5State();
+}
+
+class _Part5State extends State<Part5> {
+  final myController = TextEditingController();
+
+  //final _height = 40;
+  bool _autovalidate = false;
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 0),
+      child: Container(
+        
+        height: MediaQuery.of(context).size.height- 100,
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                //color: Colors.amber,
+                //padding: const EdgeInsets.only(left: 200,right:200),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SelectableText(
+                      'TEST OUR LATEST FEATURES.',
+                      style: TextStyle(
+                          fontSize: 45,
+                          decoration: TextDecoration.none,
+                          color: my_pink,
+                          fontFamily: 'Segoe',
+                          fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    ConstrainedBox(
+                      
+                      constraints: BoxConstraints(
+                        maxWidth: 400,
+                      ),
+                      child: Container(
+                          child: SelectableText(
+                        'Unsere Web-App ist ständig im Umbruch und wir benötigen deine Hilfe, um unsere Features zu testen.'
+                        ' Hinterlasse hier deine E-Mail Adresse, um zun den ertsen Usern gehören zu können. Damit hilfst'
+                        ' du uns beim Testen un der Entwicklung neuer Features. Wir freuen uns von dir zu hören.',
+                        style: TextStyle(
+                          fontSize: 17,
+                          decoration: TextDecoration.none,
+                          color: Colors.white,
+                          fontFamily: 'Segoe',
+                        ),
+                      )),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                        width: 400,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 4,
+                              child: Form(
+                                key: _formKey,
+                                autovalidate: _autovalidate,
+                                child: Column(
+                                  children: [
+                                    TextFormField(
+                                      decoration: InputDecoration(
+                                          errorMaxLines: 4,
+                                          errorStyle:
+                                              TextStyle(color: Colors.white),
+                                          filled: true,
+                                          fillColor: my_pink,
+                                          //labelText: 'Email',
+                                          enabledBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                              color: my_pink,
+                                            ),
+                                            borderRadius:
+                                                BorderRadius.circular(2.0),
+                                          ),
+                                          hintText: 'Deine Email...',
+                                          hintStyle: TextStyle(
+                                              color: Colors.white
+                                                  .withOpacity(0.75)),
+                                          focusedBorder: OutlineInputBorder(
+                                              borderSide:
+                                                  BorderSide(color: my_pink),
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(2))),
+                                          labelStyle: TextStyle(
+                                            color: Colors.white,
+                                          )),
+
+                                      //obscureText: true,
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20),
+                                      validator: (input) {
+                                        if (input.isEmpty |
+                                            !EmailValidator.validate(input)) {
+                                          return 'Bitte gebe eine gültige Email Adresse an';
+                                        }
+                                        return null;
+                                      },
+                                      controller: myController,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 1,
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: SizedBox(
+                                  width: double.maxFinite,
+                                  height: 59,
+                                  child: RaisedButton(
+                                    color: my_blue,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(2.0),
+                                      //side: BorderSide(color: Colors.red),
+                                    ),
+                                    onPressed: () async {
+                                      String email = myController.text;
+                                      print(email);
+                                      if (_formKey.currentState.validate()) {
+                                        firestore
+                                            .collection("emails")
+                                            .add({"email": email});
+                                        Toast.show(
+                                          "Du wurdest erfolgreich hinzugefügt",
+                                          context,
+
+                                          duration: 2,
+                                          gravity: Toast.TOP,
+
+                                          backgroundColor: my_pink,
+                                          textColor: Colors.white,
+
+                                        );
+
+                                      } else {
+                                        setState(() {
+                                          _autovalidate = true;
+                                        });
+                                      }
+                                    },
+                                    child: Icon(
+                                      Icons.email_outlined,
+                                      color: Colors.white,
+                                      size: 30,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ))
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Widgets/Mobil/body_parts/part_6.dart
@@ -0,0 +1,164 @@
+import 'package:ceenes/assets/Styles/Colors/colors.dart';
+import 'package:ceenes/assets/Styles/Icons/my_icons.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Part6 extends StatefulWidget {
+  @override
+  _Part6State createState() => _Part6State();
+}
+
+class _Part6State extends State<Part6> {
+  _launchURL() async {
+  const url = 'https://de.linkedin.com/in/benjamin-kasten-a68466155?challengeId=AQGWWfDdKCKNjwAAAXYVZyJsoBJBTAUesYA_Y30jgQvYM8XZnLmkfnDvN58rnfxhg077ug-e2Nqb_PqTIvsQiITK9rtxoP1jFw&submissionId=ab2c09ea-1410-4c16-c6a2-30032c387a20';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+  _launchURL2() async {
+  const url = 'https://de.linkedin.com/in/lorenz-pott-156a6513b';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //color: Colors.green,
+      
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              
+              //color: Colors.amber,
+
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SelectableText(
+                    'ABOUT US.',
+                    style: TextStyle(
+                        fontSize: 45,
+                        decoration: TextDecoration.none,
+                        color: my_pink,
+                        fontFamily: 'Segoe',
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 400),
+                    child: SelectableText.rich(
+                      TextSpan(
+                          text:
+                              'Wir sind zwei Wirtschaftsinformatikstudenten der Universität Paderborn. '
+                              'Wir wollen mit diesem Projekt den Freiraum, den uns die der Lehrstuhl für Digitale Märkte '
+                              'anbietet, nutzen, und unserer Kreativität freien Lauf lassen.\n'
+                              'Deshalb danken wir insbesondere Frau Seutter und Herrn Kundisch, die uns tatkräfitg zur Seite stehen '
+                              'und bei der Umsetzung dieses Projekt unterstützen.\n'
+                              'Für Kontaktanfragen sendet uns bitte eine Mail an ',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 17,
+                              decoration: TextDecoration.none,
+                              fontFamily: 'Segoe'),
+                          children: [
+                            TextSpan(
+                              text: 'ceenes.app@gmail.com',
+                              style: TextStyle(
+                                  color: my_pink,
+                                  fontSize: 17,
+                                  decoration: TextDecoration.none,
+                                  fontFamily: 'Segoe'),
+                            )
+                          ]),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 150),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        //profil Loro
+                        GestureDetector(
+                          onTap:_launchURL2,
+                          child:                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              CircleAvatar(
+                                backgroundColor: Colors.transparent,
+                                backgroundImage: AssetImage(profil_loro),
+                                radius: 70,
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              
+                              Text(
+                                'Lorenz P.',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              Container(
+                                height: 20,
+                                child:Image.asset(linkedIn)
+                                ),
+                              
+                            ],
+                          ),
+                        ),
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        //Profil benji
+                        GestureDetector(
+                          onTap:_launchURL,
+                          child:Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              CircleAvatar(
+                                backgroundColor: Colors.transparent,
+                                backgroundImage: AssetImage(profil_benji),
+                                radius: 70,
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                'Benjamin K.',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              Container(
+                                height: 20,
+                                child:Image.asset(linkedIn)
+                              ),
+                            ],
+                          ),
+                        )
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
